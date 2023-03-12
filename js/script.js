430f9fa54d5dde7bf6d9053b2beb4c2018adc4ec
Niran Yousuf <niran069@gmail.com>
2023-03-12 20:21:57 +0600

    Banner update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 (function ($) {
     "user strict";
+    $(document).ready(function () {
+        // mobile menu
+        $('.products-menu').append($('.w-mega-menu'))
+        $(".main_menu").prepend(`
+            <div class="mobile_logo_closeButton">
+                <div class="logo"><a href="#">Coding <span>Tutor</span></a></div>
+                <button id="close_menu" class="close_menu"></button>
+            </div>`);
 
-    // $(window).on('scroll', function () {
-    //     if ($(this).scrollTop() > 36) {
-    //         $('.navbar').addClass('sticky');
-    //     } else {
-    //         $('.navbar').removeClass('sticky');
-    //     }
-    // });
+        $(".navbar-toggler").on("click", () => {
+            $(".main_menu").addClass("open_menu");
+        });
+
+        $(".close_menu").on("click", () => {
+            $(".main_menu").removeClass("open_menu");
+        });
+
+
+        if ($(window).width() < 992) {
+            $(".products-menu").on("click", function () {
+                $(this).find(".w-mega-menu").slideToggle();
+            });
+        }
+
+
+
+    });
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 36) {
+            $('.navbar').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky');
+        }
+    });
 
     $('.owl-carousel').owlCarousel({
         loop: true,
