d63e83aa3f2822049e27999c1232731d05edf41a
Niran Yousuf <niran069@gmail.com>
2023-12-12 17:36:53 +0600

    working on like

--- single-professor.php
+++ single-professor.php
@@ -21,11 +21,30 @@ pageBanner();
                                 <div class="col-lg-4">
                                     <div class="featured-media">
                                         <?php the_post_thumbnail('professorPortrait'); ?>
+                                        
+                                        <?php
+                                        $likeCount = new WP_Query(array (
+                                            'post_type' => 'like',
+                                            'meta_query' => array(
+                                                array (
+                                                    'key' => 'liked_professor_id',
+                                                    'compare' => '=',
+                                                    'value' => get_the_ID()
+                                                )
+                                            )
+                                        ));
+                                        ?>
+                                        <div class="like-box">
+                                            <span class="icon icon-thumbs-up-alt" aria-hidden="true"></span>
+                                            <span class="icon icon-thumbs-up" aria-hidden="true"></span>
+                                            <span class="like-count"><?php echo $likeCount->found_posts; ?></span>
+                                        </div>
                                     </div>
                                 </div>
 
                                 <div class="col-lg-8">
                                     <?php the_content(); ?>
+
                                 </div>
                             </div>
                         </div>
