e5e0d83e4eea1300ebddb8aa7e8d308e6dc025f3
Niran Yousuf <niran069@gmail.com>
2024-08-13 09:33:41 +0600

    Update like-route.php

--- inc/like-route.php
+++ inc/like-route.php
@@ -1,2 +1,22 @@
 <?php
 
+add_action('rest_api_init', 'ct_like_routes');
+function ct_like_routes() {
+	register_rest_route('ct/v1', 'manageLike', array(
+		'methods' => 'POST',
+		'callback' => 'createLike'
+	));
+	register_rest_route('ct/v1', 'manageLike', array(
+		'methods' => 'DELETE',
+		'callback' => 'deleteLike'
+	));
+}
+
+
+function createLike() {
+	return 'Thanks for trying to create a like. ';
+}
+
+function deleteLike() {
+	return 'Thanks for trying to delete the like. ';
+}
