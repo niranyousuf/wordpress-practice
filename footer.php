e90df1d0d8086999a045ad8523acf5c18272ff2d
Niran Yousuf <niran069@gmail.com>
2025-01-28 23:18:31 +0600

    footer style update

--- footer.php
+++ footer.php
@@ -1,55 +1,42 @@
-<footer id="footer" style="display: none;">
+<footer class="footer">
   <div class="container">
-    <div class="row">
-      <div class="col-lg-7">
-        <div class="product-info">
-          <a href="<?php echo site_url() ?>" class="logo">
-            <?php get_template_part('svgs/logo'); ?>
-          </a>
-          <p>Experience remarkable WordPress products with a new level of power, beauty, and human-centered designs. Think you know WordPress products? Think deeper!</p>
-        </div>
+    <div class="footer_wrapper">
+      <div class="product-info">
+        <a href="<?php echo site_url() ?>" class="logo">
+          <?php get_template_part('svgs/logo'); ?>
+        </a>
+        <p>Experience remarkable WordPress products with a new level of power, beauty, and human-centered designs. Think you know WordPress products? Think deeper!</p>
       </div>
-
-      <div class="col-lg-5">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="policies footer-links">
-              <h3>Pages</h3>
-              <ul class="links">
-                <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-                <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy policy</a></li>
-                <li><a href="#">Refund policy</a></li>
-                <li><a href="#">Terms & conditions</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="social-media footer-links">
-              <h3>Social media</h3>
-              <ul class="links">
-                <li><a href="#"><span class="icon icon-facebook"></span> Facebook</a></li>
-                <li><a href="#"><span class="icon icon-twitter"></span> Twitter</a></li>
-                <li><a href="#"><span class="icon icon-linkedin"></span> Linkedin</a></li>
-                <li><a href="#"><span class="icon icon-instagram"></span> Instagram</a></li>
-              </ul>
-            </div>
-          </div>
+      <div class="footer_menu">
+        <div class="policies footer-links">
+          <h3>Pages</h3>
+          <ul class="links">
+            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+            <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy policy</a></li>
+            <li><a href="<?php echo site_url('/privacy-policy') ?>">Refund policy</a></li>
+            <li><a href="<?php echo site_url('/privacy-policy') ?>">Terms & conditions</a></li>
+          </ul>
+        </div>
+        <div class="social-media footer-links">
+          <h3>Social media</h3>
+          <ul class="links">
+            <li><a href="#"><span class="icon icon-facebook"></span> Facebook</a></li>
+            <li><a href="#"><span class="icon icon-twitter"></span> Twitter</a></li>
+            <li><a href="#"><span class="icon icon-linkedin"></span> Linkedin</a></li>
+            <li><a href="#"><span class="icon icon-instagram"></span> Instagram</a></li>
+          </ul>
         </div>
       </div>
     </div>
 
-    <div class="row">
-      <div class="col-12">
-        <div class="copyright">
-          <p>&copy; <?php echo date('Y') ?> CodeMonster | All Rights Reserved</p>
-        </div>
-      </div>
+    <div class="copyright">
+      <p>&copy; <?php echo date('Y') ?> <?php bloginfo(); ?> | All Rights Reserved</p>
     </div>
   </div>
 </footer>
 
 
-<?php  get_template_part('template-parts/content', 'search'); ?>
+<?php get_template_part('template-parts/content', 'search'); ?>
 
 <?php wp_footer(); ?>
 </body>
