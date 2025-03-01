d6df742ee7cc7f9bec7c8e5d2fafb34cc738b0ad
Niran Yousuf <niran069@gmail.com>
2025-03-01 07:39:20 +0600

    search page updae

--- search.php
+++ search.php
@@ -12,28 +12,24 @@ pageBanner(array(
 ?>
 <section class="post-page">
   <div class="container">
-    <div class="row">
-      <?php
-      if (have_posts()) {
-        while (have_posts()) {
-          the_post();
-          get_template_part('template-parts/content', get_post_type());
-        }
-      } else {
-        _e('<h2>No result match that serch</h2>');
+  <div class="content-block">
+    <?php
+    if (have_posts()) {
+      while (have_posts()) {
+        the_post();
+        get_template_part('template-parts/content', get_post_type());
       }
+    } else {
+      _e('<h2>No result match that serch</h2>');
+    }
 
-      get_search_form();
-      ?>
+    get_search_form();
+    ?>
 
-      <div class="col-md-12">
-        <div class="pagination max_container">
-          <?php echo paginate_links(); ?>
-        </div>
-      </div>
+    <div class="pagination max_container">
+      <?php echo paginate_links(); ?>
     </div>
-
-
+  </div>
   </div>
 </section>
 
