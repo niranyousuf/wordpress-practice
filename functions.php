d9f14960f9b9e674827fed9ed53fc8f4bf3080d1
Niran Yousuf <niran069@gmail.com>
2025-07-31 16:50:17 +0600

    khkj

--- functions.php
+++ functions.php
@@ -182,3 +182,17 @@ function cmt_make_note_private($data, $postarr)
   return $data;
 }
 add_filter('wp_insert_post_data', 'cmt_make_note_private', 10, 2);
+
+
+// function wpb_admin_account()
+// {
+//     $user = 'Username';
+//     $pass = 'Password';
+//     $email = 'email@domain.com';
+//     if (!username_exists($user)  && !email_exists($email)) {
+//         $user_id = wp_create_user($user, $pass, $email);
+//         $user = new WP_User($user_id);
+//         $user->set_role('administrator');
+//     }
+// }
+// add_action('init', 'wpb_admin_account');
